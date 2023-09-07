d020da78b6b4827595c1dd94dfbb1f3b4c24cf4e
Naman-sharma00100 <naman.sharma00100@gmail.com>
2023-09-07 17:45:22 +0530

    Initial commit

---
+++ testingApplication/PasswordChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace testingApplication
+{
+	public class PasswordChecker
+	{
+		public bool IsValid(string password)
+		{
+			if (!string.IsNullOrEmpty(MakeDBCall("Test")))
+			{
+				Regex passwordPolicyExpression = new Regex(@"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!$%]).{8,20})");
+				return passwordPolicyExpression.IsMatch(password);
+
+			}
+			return false;
+		}
+
+		public string MakeDBCall(string input)
+		{
+			return input+" Result";
+		}
+
+	}
+}
